4082048893c8de81e09b1246c149573f1363d114
Sa-raCha <sarabibimagdalena@gmail.com>
2022-03-21 14:29:32 +0100

    Making a connection with the database

--- Starter-pack/Classes/DatabaseManager.php
+++ Starter-pack/Classes/DatabaseManager.php
@@ -29,6 +29,7 @@ class DatabaseManager
             $dsn = "mysql:host={$this->host};port=8889;dbname={$this->dbname}";
             $this->connection = new PDO($dsn, $this->user, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "succes";
          }
         catch (PDOException $exception){
             echo "message ", $exception->getMessage();
